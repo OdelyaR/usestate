01a994a56ee5f9060dd4e1d96a42b54516f80a14
OdelyaR <odeleya.rr@gmail.com>
2022-11-26 23:15:58 +0200

    resest button

--- src/Counter.js
+++ src/Counter.js
@@ -9,11 +9,15 @@ function Counter() {
         )
         console.log(count)
     }
+    function rst(){
+        setCount(0)
+    }
     return (
       <div>
         <h1>Counter</h1>
         <p>Counter is at {count}</p>
         <button onClick={incr}>Click to add 1 to Counter</button>
+        <button onClick={rst}>Reset Counter</button>
       </div>
     );
   }
